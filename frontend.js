e48d9acfe2e3716ddce49fde98c696bd4fb7f717
Alex Yong <ajyong@globalnoc.iu.edu>
2020-01-26 00:17:44 -0500

    Slight code adjustments and updating some comments.

--- frontend.js
+++ frontend.js
@@ -1,143 +1,148 @@
 $( document ).ready(function() {
 
+    //open hyperlinks on the UI in the browser.
 	$(document).on('click', 'a[href^="http"]', function(event) {
         event.preventDefault();
         window.Electron.shell.openExternal(this.href);
     });
-        $('#modal_show').click(function() {
-            $('#modal1').modal('show')
-        });
-        $('#modal2_show').click(function() {
-            $('#modal2').modal('show')
-        });
-        $('#do_it_button').click(function(event) {
-            delete_fb_stuff = require('./delete_fb_stuff');
-            event.preventDefault();
-            //run the facebook module
-            //
 
-            var username = $("#username").val();
-            var password = $("#password").val();
-            var twofactor = $("#twofactor").val();
+    $('#modal_show').click(function() {
+        $('#modal1').modal('show')
+    });
+    $('#modal2_show').click(function() {
+        $('#modal2').modal('show')
+    });
 
-            if (!username || !password || username == "" || password == "") {
-                alert("Username and password are required yo.");
+    $('#do_it_button').click(function(event) {
+        delete_fb_stuff = require('./delete_fb_stuff');
+        event.preventDefault();
+        //run the facebook module
+        //
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var twofactor = $("#twofactor").val();
+
+        if (!username || !password || username == "" || password == "") {
+            alert("Username and password are required yo.");
+            return;
+        }
+
+        var categories = $("#categories").val();
+        if (!categories || categories =="") {
+            alert("You need to select some categories yo.");
+            return;
+        }
+        //categories = categories.join(" ");
+        var years = $("#years").val();
+        if (!years || years =="") {
+            alert("You need to select some years yo.");
+            return;
+        }
+        //years = years.join(" ");
+        var months = $("#months").val();
+        if (!months || months == "") {
+            alert("You need to select some months yo.");
+            return;
+        }
+
+        if (twofactor) {
+            var r = confirm("Two factor support in the application at this time is a bit rough, and may not work. You may be better off disabling two factor on your account, then trying to run this script. Are you sure you want to continue?");
+            if (r){
+
+            }
+            else {
                 return;
             }
 
-            var categories = $("#categories").val();
-            if (!categories || categories =="") {
-                alert("You need to select some categories yo.");
-                return;
-            }
-            //categories = categories.join(" ");
-            var years = $("#years").val();
-            if (!years || years =="") {
-                alert("You need to select some years yo.");
-                return;
-            }
-            //years = years.join(" ");
-            var months = $("#months").val();
-            if (!months || months == "") {
-                alert("You need to select some months yo.");
-                return;
-            }
+        }
 
-            if (twofactor) {
-                var r = confirm("Two factor support in the application at this time is a bit rough, and may not work. You may be better off disabling two factor on your account, then trying to run this script. Are you sure you want to continue?");
-                if (r){
 
+        $("#status").val("Running script...");
+
+        try {
+            var result = delete_fb_stuff.main(username,password,categories,years,months,twofactor).then(function(value) {
+
+                if (!value) {
+                    $("#status").val("Fatal error running script!");
                 }
                 else {
-                    return;
+
+                    $("#status").val(value.message);
                 }
 
-            }
+            });
 
-
-            $("#status").val("Running script...");
-
-            try {
-                var result = delete_fb_stuff.main(username,password,categories,years,months,twofactor).then(function(value) {
-
-                    if (!value) {
-                        $("#status").val("Fatal error running script!");
-                    }
-                    else {
-
-                        $("#status").val(value.message);
-                    }
-
-                });
-
-            }
-            catch(err) {
-                $("#status").val(err);
-            }
-        });
-        //the below code goes and builds up the select options.
-        var currentYear = (new Date()).getFullYear();
-        var years = [];
-        for (let i = 2004; i <= currentYear; i++) {
-          years.push(i.toString());
         }
-        years.reverse();
-
-        //add years
-        for (var i = 0; i< years.length; i++) {
-            var x = document.getElementById("years");
-            var option = document.createElement("option");
-            option.text = years[i];
-            option.value = years[i];
-            x.add(option);
+        catch(err) {
+            $("#status").val(err);
         }
+    });
 
-       	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        //add months
-        for (var i = 0; i< months.length; i++) {
-            var x = document.getElementById("months");
-            var option = document.createElement("option");
-            option.text = months[i];
-            option.value = months[i];
-            x.add(option);
-        }
 
-		var categories = ["Posts",
-		  "Posts You're Tagged In",
-		  "Photos and Videos",
-		  "Photos You're Tagged In",
-		  "Others' Posts To Your Timeline",
-		  "Hidden From Timeline",
-		  "Likes and Reactions",
-		  "Comments",
-		  "Profile",
-		  "Added Friends",
-		  "Life Events",
-		  "Songs You've Listened To",
-		  "Articles You've Read",
-		  "Movies and TV",
-		  "Games",
-		  "Books",
-		  "Products You Wanted",
-		  "Notes",
-		  "Videos You've Watched",
-		  "Following",
-		  "Groups",
-		  "Events",
-		  "Polls",
-		  "Search History",
-		  "Saved",
-          "Apps"]; 
+    //the below code goes and builds up the select options.
+    var currentYear = (new Date()).getFullYear();
+    var years = [];
+    for (let i = 2004; i <= currentYear; i++) {
+      years.push(i.toString());
+    }
+    years.reverse();
 
-        for (var i = 0; i< categories.length; i++) {
-            var x = document.getElementById("categories");
-            var option = document.createElement("option");
-            option.text = categories[i];
-            option.value = categories[i];
-            x.add(option);
-        }
+    //add years
+    for (var i = 0; i< years.length; i++) {
+        var x = document.getElementById("years");
+        var option = document.createElement("option");
+        option.text = years[i];
+        option.value = years[i];
+        x.add(option);
+    }
 
-        $('select').selectpicker({actionsBox: true});
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    //add months
+    for (var i = 0; i< months.length; i++) {
+        var x = document.getElementById("months");
+        var option = document.createElement("option");
+        option.text = months[i];
+        option.value = months[i];
+        x.add(option);
+    }
+
+    var categories = ["Posts",
+      "Posts You're Tagged In",
+      "Photos and Videos",
+      "Photos You're Tagged In",
+      "Others' Posts To Your Timeline",
+      "Hidden From Timeline",
+      "Likes and Reactions",
+      "Comments",
+      "Profile",
+      "Added Friends",
+      "Life Events",
+      "Songs You've Listened To",
+      "Articles You've Read",
+      "Movies and TV",
+      "Games",
+      "Books",
+      "Products You Wanted",
+      "Notes",
+      "Videos You've Watched",
+      "Following",
+      "Groups",
+      "Events",
+      "Polls",
+      "Search History",
+      "Saved",
+      "Apps"]; 
+
+    for (var i = 0; i< categories.length; i++) {
+        var x = document.getElementById("categories");
+        var option = document.createElement("option");
+        option.text = categories[i];
+        option.value = categories[i];
+        x.add(option);
+    }
+
+    $('select').selectpicker({actionsBox: true});
 });
 function selectAll(selectBox,selectAll) { 
     // have we been passed an ID 
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600, icon: path.join(__dirname, 'assets/icons/png/icon_32x32@2x.png')})
 
-    //quite win the windows are closed.
+    //quit when the windows are closed.
     mainWindow.on('closed', () => {
         mainWindow = null;
         app.quit();
@@ -20,14 +20,6 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
 
     mainWindow.on('will-navigate', handleRedirect)
     mainWindow.on('new-window', handleRedirect)
